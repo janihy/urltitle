0ea7cc8b9a9e3a00564b9848e6b5a0b72341e002
Jani Hyytiäinen <janihy@gmail.com>
2018-08-21 20:03:39 +0300

    add bind for spotify uris

--- urltitle.tcl
+++ urltitle.tcl
@@ -49,6 +49,7 @@ namespace eval UrlTitle {
 
   # BINDS
   bind pubm "-|-" {*://*} UrlTitle::handler
+  bind pubm "-|-" {spotify:*} UrlTitle::handler
   setudef flag urltitle        ;# Channel flag to enable script.
   setudef flag logurltitle     ;# Channel flag to enable logging of script.
 
